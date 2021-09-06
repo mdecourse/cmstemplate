0fd40fe6894e78c49dbcc687903a0445723c4089
mdecourse <mdecourse@gmail.com>
2021-09-06 22:03:33 +0800

    update cmsimde to integrate http-server.py into dynamic site

--- init.py
+++ init.py
@@ -10,9 +10,10 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "CMSTemplate"
+    site_title = "CMSiMDE"
     ip = "127.0.0.1"
-    port = 9445
+    dynamic_port = 9444
+    static_port = 8444
     def __init__(self):
         # hope to create downloads and images directories　
         if not os.path.isdir(_curdir + "/downloads"):
